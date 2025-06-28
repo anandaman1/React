6fd6b15c0862aadc507ca03811a5227065e574e8
Aman Anand <aman1001anand@gmail.com>
2025-06-29 02:51:20 +0530

    initial com

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const element = React.createElement("h1", {}, "Hello Aman" );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const element = React.createElement("h1",{key: "e1", id:"first", className:"Rahul", style:{backgroundColor:"blue", fontSize:"30px", color:"pink"}},"Hello Coder Army");
+const element2 = React.createElement("h2",{key: "e1",id:"second", className:"Rahul", style:{backgroundColor:"black", fontSize:"30px", color:"pink"}},"Maja aaya muje");
+
+
+
+
+const div1 = React.createElement('div',{},[element, element2]); 
+const Reactroot = ReactDOM.createRoot(document.getElementById('root'));
+
+Reactroot.render(div1);
+
+
+
+
